804d346368cdab511484be6fd3c6397d63217fad
kar-21 <karthiks210198@gmail.com>
2022-06-20 16:48:22 +0530

    Adding material UI and material icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SavedNotes from "./pages/SavedNotes";
+import NavigationHeader from "./templates/NavigationHeader";
 import "./App.scss";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Suspense>
+        <NavigationHeader />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/saved" element={<SavedNotes />} />

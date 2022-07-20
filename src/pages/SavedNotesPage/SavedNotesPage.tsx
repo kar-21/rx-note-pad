311d5f1e1f7ea98024b6615eeea7a657ef4cd5b1
kar-21 <karthiks210198@gmail.com>
2022-07-20 12:05:56 +0530

    Adding render of notes from redux state

--- src/pages/SavedNotesPage/SavedNotesPage.tsx
+++ src/pages/SavedNotesPage/SavedNotesPage.tsx
@@ -1,9 +1,16 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/Reducers";
 import NoteCard from "../../templates/NoteCard/NoteCard";
 
 const SavedNotes = () => {
+  const notes = useSelector((state: RootState) => state.notesReducer);
+
   return (
     <div className="saved-notes-page-container">
       <h1>Saved Notes</h1>
+      {Object.values(notes).map((note) => (
+        <NoteCard key={note.id} isNewCard={false} noteFromRedux={note} />
+      ))}
       <NoteCard isNewCard />
     </div>
   );

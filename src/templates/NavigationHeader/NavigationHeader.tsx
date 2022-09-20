94a642ec1559d41a910ce515fb129873b7e4d5fe
kar-21 <karthiks210198@gmail.com>
2022-09-20 11:33:03 +0530

    Adding side nav bar

--- src/templates/NavigationHeader/NavigationHeader.tsx
+++ src/templates/NavigationHeader/NavigationHeader.tsx
@@ -1,4 +1,12 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -13,33 +21,53 @@ const NavigationHeader = () => {
     (state: RootState) => state.userDetailsReducer
   );
 
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setIsDrawerOpen(open);
+    };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Note Pad
-        </Typography>
-        {jwtToken ? (
-          <span className="name-container">
-            <AccountCircleIcon />
-            {given_name}
-          </span>
-        ) : (
-          <Button color="inherit" onClick={() => navigate("/login")}>
-            Login
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Note Pad
+          </Typography>
+          {jwtToken ? (
+            <span className="name-container">
+              <AccountCircleIcon />
+              {given_name}
+            </span>
+          ) : (
+            <Button color="inherit" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        Opened Sid Navigation
+      </Drawer>
+    </>
   );
 };
 

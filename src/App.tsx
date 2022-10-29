6c192213f1abf1fc96cefad9c6697fc4d217e128
kar-21 <karthiks210198@gmail.com>
2022-10-29 17:40:56 +0530

    netlify configurations for redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import store from "./store/store";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import MyNotes from "./pages/MyNotesPage/MyNotesPage";
@@ -11,20 +13,22 @@ import GetUserInfo from "./templates/GetUserInfo/GetUserInfo";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Suspense>
-          <NavigationHeader />
-          <GetUserInfo />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/my-notes" element={<MyNotes />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/token/:token" element={<TokenPage />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Suspense>
+            <NavigationHeader />
+            <GetUserInfo />
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/my-notes" element={<MyNotes />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/token/:token" element={<TokenPage />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </Provider>
+    </SafeAreaProvider>
   );
 };
 
